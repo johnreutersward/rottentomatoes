dc41a3fbdf46b6909d7d3ff7f816d5e2c207c7f5
rojters <rojters@gmail.com>
2013-12-04 16:35:48 +0100

    more MoviesInfo test

--- movies.go
+++ movies.go
@@ -1,55 +1,72 @@
 package rottentomatoes
 
 import (
-    "net/url"
-    "bytes"
-    "encoding/json"
-    "text/template"
-    "errors"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/url"
+	"text/template"
 )
 
 type Movie struct {
-    Id int `json:"id"`
-    Title string `json:"title"`
+	Id               int      `json:"id"`
+	Title            string   `json:"title"`
+	Year             int      `json:"year"`
+	Genres           []string `json:"genres"`
+	MPAA_Rating      string   `json:"mpaa_rating"`
+	Runtime          int      `json:"runtime"`
+	CriticsConsensus string   `json:"critics_consensus"`
 }
 
+type ReleaseDates struct {
+	Theater string `json:"theater"`
+	DVD     string `json:"dvd"`
+}
+
+type Ratings struct {
+	CriticsRating  string `json:"critics_rating"`
+	CriticsScore   int    `json:"critics_score"`
+	AudienceRating string `json:"audience_rating"`
+	AudienceScore  int    `json:"audience_score"`
+}
 
 func UnmarshalMoviesInfo(data []byte) (Movie, error) {
-    var m Movie
-    //m := new(Movie)
-    err := json.Unmarshal(data, &m)
+	var m Movie
+	//m := new(Movie)
+	err := json.Unmarshal(data, &m)
 
-    return m, err
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return m, err
 }
-
 
 func (c *Client) MoviesInfo(id string) (Movie, error) {
 
-    var movie Movie
+	var movie Movie
 
-    if len(c.ApiKey) == 0  {
-        return movie, errors.New("missing ApiKey")
-    }
+	if len(c.ApiKey) == 0 {
+		return movie, errors.New("missing ApiKey")
+	}
 
-    t, _ := template.New("MoviesInfoUrl").Parse(c.BaseUrl["MoviesInfo"])
-    buf := new(bytes.Buffer)
-    t.Execute(buf, id)
+	t, _ := template.New("MoviesInfoUrl").Parse(c.BaseUrl["MoviesInfo"])
+	buf := new(bytes.Buffer)
+	t.Execute(buf, id)
 
-    v := url.Values {}
-    v.Set("apikey", c.ApiKey)
+	v := url.Values{}
+	v.Set("apikey", c.ApiKey)
 
-    endp := buf.String() + v.Encode()
+	endp := buf.String() + v.Encode()
 
-    data, err := c.Request(endp)
-    
-    if err != nil {
-        return movie, err
-    }
-    
-    movie, err = UnmarshalMoviesInfo(data) 
+	data, err := c.Request(endp)
 
-    return movie, err
+	if err != nil {
+		return movie, err
+	}
+
+	movie, err = UnmarshalMoviesInfo(data)
+
+	return movie, err
 }
-
-
-
--- movies_test.go
+++ movies_test.go
@@ -1,26 +1,50 @@
 package rottentomatoes
 
 import (
-	"testing"
+	"log"
 	"reflect"
+	"testing"
 )
 
 func TestUnmarshalMoviesInfo(t *testing.T) {
 	data := `{
 	    "id": 14281,
-    	"title": "The Big Lebowski"
+    	"title": "The Big Lebowski",
+    	"year": 1998,
+		"genres": ["Comedy"],
+	    "mpaa_rating": "R",
+	    "runtime": 118,
+	    "critics_consensus": "",
+	    "release_dates": {
+	        "theater": "1998-03-06",
+	        "dvd": "1998-10-27"
+    	},
+    	"ratings": {
+	        "critics_rating": "Certified Fresh",
+	        "critics_score": 80,
+	        "audience_rating": "Upright",
+	        "audience_score": 94
+    	}
 	}`
-	
-	got, _ := UnmarshalMoviesInfo([]byte(data))
 
-	want := Movie {
-		Id: 14281,
-		Title: "The Big Lebowski",
+	got, err := UnmarshalMoviesInfo([]byte(data))
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	want := Movie{
+		Id:               14281,
+		Title:            "The Big Lebowski",
+		Year:             1998,
+		Genres:           []string{"Comedy"},
+		MPAA_Rating:      "R",
+		Runtime:          118,
+		CriticsConsensus: "",
 	}
 
 	if !reflect.DeepEqual(got, want) {
-    	t.Errorf("UnmarshalMoviesInfo = %+v, want %+v", got, want)
-    }
-
+		t.Errorf("UnmarshalMoviesInfo = %+v,\nwant %+v", got, want)
+	}
 
 }
