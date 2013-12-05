62caac57518458484f66af5053e8eef6cbf42040
rojters <rojters@gmail.com>
2013-12-05 04:22:31 +0100

    MoviesInfo done, all tests pass

--- movies.go
+++ movies.go
@@ -10,13 +10,22 @@ import (
 )
 
 type Movie struct {
-	Id               int      `json:"id"`
-	Title            string   `json:"title"`
-	Year             int      `json:"year"`
-	Genres           []string `json:"genres"`
-	MPAA_Rating      string   `json:"mpaa_rating"`
-	Runtime          int      `json:"runtime"`
-	CriticsConsensus string   `json:"critics_consensus"`
+	Id                int          `json:"id"`
+	Title             string       `json:"title"`
+	Year              int          `json:"year"`
+	Genres            []string     `json:"genres"`
+	MPAA_Rating       string       `json:"mpaa_rating"`
+	Runtime           int          `json:"runtime"`
+	CriticsConsensus  string       `json:"critics_consensus"`
+	ReleaseDates      ReleaseDates `json:"release_dates"`
+	Ratings           Ratings      `json:"ratings"`
+	Synopsis          string       `json:"synopsis"`
+	Posters           Posters      `json:"posters"`
+	AbridgedCast      []Cast       `json:"abridged_cast"`
+	AbridgedDirectors []Director   `json:"abridged_directors"`
+	Studio            string       `json:"studio"`
+	AlternateIds      AlternateIds `json:"alternate_ids"`
+	Links             Links        `json:"links"`
 }
 
 type ReleaseDates struct {
@@ -29,6 +38,30 @@ type Ratings struct {
 	CriticsScore   int    `json:"critics_score"`
 	AudienceRating string `json:"audience_rating"`
 	AudienceScore  int    `json:"audience_score"`
+}
+
+type Posters struct {
+	Thumbnail string `json:"thumbnail"`
+	Profile   string `json:"Profile"`
+	Detailed  string `json:"Detailed"`
+	Original  string `json:"original"`
+}
+
+type Director struct {
+	Name string `json:"name"`
+}
+
+type AlternateIds struct {
+	IMDB string `json:"imdb"`
+}
+
+type Links struct {
+	Self      string `json:"self"`
+	Alternate string `json:"alternate"`
+	Cast      string `json:"cast"`
+	Clips     string `json:"clips"`
+	Reviews   string `json:"reviews"`
+	Similar   string `json:"similar"`
 }
 
 func UnmarshalMoviesInfo(data []byte) (Movie, error) {
--- movies_test.go
+++ movies_test.go
@@ -24,7 +24,52 @@ func TestUnmarshalMoviesInfo(t *testing.T) {
 	        "critics_score": 80,
 	        "audience_rating": "Upright",
 	        "audience_score": 94
-    	}
+    	},
+	    "synopsis": "",
+	    "posters": {
+	        "thumbnail": "http://content6.flixster.com/movie/10/95/21/10952108_mob.jpg",
+	        "profile": "http://content6.flixster.com/movie/10/95/21/10952108_pro.jpg",
+	        "detailed": "http://content6.flixster.com/movie/10/95/21/10952108_det.jpg",
+	        "original": "http://content6.flixster.com/movie/10/95/21/10952108_ori.jpg"
+	    },
+	    "abridged_cast": [{
+	        "name": "Jeff Bridges",
+	        "id": "162655890",
+	        "characters": ["The Dude"]
+	    }, {
+	        "name": "John Goodman",
+	        "id": "162655706",
+	        "characters": ["Walter Sobchak"]
+	    }, {
+	        "name": "Julianne Moore",
+	        "id": "162654248",
+	        "characters": ["Maude Lebowski"]
+	    }, {
+	        "name": "Steve Buscemi",
+	        "id": "162652875",
+	        "characters": ["Donny"]
+	    }, {
+	        "name": "David Huddleston",
+	        "id": "167963972",
+	        "characters": ["The Big Lebowski"]
+	    }],
+	    "abridged_directors": [{
+	        "name": "Joel Coen"
+	    }, {
+	        "name": "Ethan Coen"
+	    }],
+	    "studio": "Gramercy Pictures",
+	    "alternate_ids": {
+	        "imdb": "0118715"
+	    },
+	    "links": {
+	        "self": "http://api.rottentomatoes.com/api/public/v1.0/movies/14281.json",
+	        "alternate": "http://www.rottentomatoes.com/m/big_lebowski/",
+	        "cast": "http://api.rottentomatoes.com/api/public/v1.0/movies/14281/cast.json",
+	        "clips": "http://api.rottentomatoes.com/api/public/v1.0/movies/14281/clips.json",
+	        "reviews": "http://api.rottentomatoes.com/api/public/v1.0/movies/14281/reviews.json",
+	        "similar": "http://api.rottentomatoes.com/api/public/v1.0/movies/14281/similar.json"
+	    }
 	}`
 
 	got, err := UnmarshalMoviesInfo([]byte(data))
@@ -41,6 +86,46 @@ func TestUnmarshalMoviesInfo(t *testing.T) {
 		MPAA_Rating:      "R",
 		Runtime:          118,
 		CriticsConsensus: "",
+		ReleaseDates: ReleaseDates{
+			Theater: "1998-03-06",
+			DVD:     "1998-10-27",
+		},
+		Ratings: Ratings{
+			CriticsRating:  "Certified Fresh",
+			CriticsScore:   80,
+			AudienceRating: "Upright",
+			AudienceScore:  94,
+		},
+		Synopsis: "",
+		Posters: Posters{
+			Thumbnail: "http://content6.flixster.com/movie/10/95/21/10952108_mob.jpg",
+			Profile:   "http://content6.flixster.com/movie/10/95/21/10952108_pro.jpg",
+			Detailed:  "http://content6.flixster.com/movie/10/95/21/10952108_det.jpg",
+			Original:  "http://content6.flixster.com/movie/10/95/21/10952108_ori.jpg",
+		},
+		AbridgedCast: []Cast{
+			Cast{Name: "Jeff Bridges", Id: "162655890", Characters: []string{"The Dude"}},
+			Cast{Name: "John Goodman", Id: "162655706", Characters: []string{"Walter Sobchak"}},
+			Cast{Name: "Julianne Moore", Id: "162654248", Characters: []string{"Maude Lebowski"}},
+			Cast{Name: "Steve Buscemi", Id: "162652875", Characters: []string{"Donny"}},
+			Cast{Name: "David Huddleston", Id: "167963972", Characters: []string{"The Big Lebowski"}},
+		},
+		AbridgedDirectors: []Director{
+			Director{Name: "Joel Coen"},
+			Director{Name: "Ethan Coen"},
+		},
+		Studio: "Gramercy Pictures",
+		AlternateIds: AlternateIds{
+			IMDB: "0118715",
+		},
+		Links: Links{
+			Self:      "http://api.rottentomatoes.com/api/public/v1.0/movies/14281.json",
+			Alternate: "http://www.rottentomatoes.com/m/big_lebowski/",
+			Cast:      "http://api.rottentomatoes.com/api/public/v1.0/movies/14281/cast.json",
+			Clips:     "http://api.rottentomatoes.com/api/public/v1.0/movies/14281/clips.json",
+			Reviews:   "http://api.rottentomatoes.com/api/public/v1.0/movies/14281/reviews.json",
+			Similar:   "http://api.rottentomatoes.com/api/public/v1.0/movies/14281/similar.json",
+		},
 	}
 
 	if !reflect.DeepEqual(got, want) {
